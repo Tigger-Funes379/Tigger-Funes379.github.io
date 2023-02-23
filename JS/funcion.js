a382d02dc8b90aa2c09688969e952fcbabc84e1c
Tigger-Funes379 <santi.funes2012arooz@gmail.com>
2023-02-23 15:23:06 -0300

    Primer commit

---
+++ JS/funcion.js
@@ -0,0 +1,24 @@
+document.getElementById('boton_mostrar1').addEventListener('click', function(){
+    document.getElementById('texto1').style.display = 'block';
+});
+document.getElementById('boton_ocultar1').addEventListener('click', function(){
+    document.getElementById('texto1').style.display = 'none';
+});
+document.getElementById('boton_mostrar2').addEventListener('click', function(){
+    document.getElementById('texto2').style.display = 'block';
+});
+document.getElementById('boton_ocultar2').addEventListener('click', function(){
+    document.getElementById('texto2').style.display = 'none';
+});
+document.getElementById('boton_mostrar3').addEventListener('click', function(){
+    document.getElementById('texto3').style.display = 'block';
+});
+document.getElementById('boton_ocultar3').addEventListener('click', function(){
+    document.getElementById('texto3').style.display = 'none';
+});
+document.getElementById('boton_mostrar4').addEventListener('click', function(){
+    document.getElementById('texto4').style.display = 'block';
+});
+document.getElementById('boton_ocultar4').addEventListener('click', function(){
+    document.getElementById('texto4').style.display = 'none';
+});
